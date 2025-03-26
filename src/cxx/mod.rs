88350d642961a90e67847d37b53240bfba8541f3
Angelos Drossos <develangel@mail.drossos.de>
2025-03-26 00:21:59 +0000

    feat: introduce type-state pattern

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -1,48 +1,113 @@
+#![allow(dead_code)]
 use anyhow::{Error, Result};
 use cxx::SharedPtr;
 
-pub struct Device(SharedPtr<intern::ffi::Device>);
-pub struct Hsm(SharedPtr<intern::ffi::HSM>);
+use std::marker::PhantomData;
 
-impl std::ops::Deref for Device {
+// ---------------------------
+// type state pattern
+// ---------------------------
+
+pub struct Device<Type = UnknownDeviceType, OS = UnknownOS>
+where
+    Type: DeviceType,
+    OS: DeviceOS,
+{
+    _num: usize,
+    dtype: Type,
+    _os: PhantomData<OS>,
+}
+
+impl Device {
+    pub fn fetch_device(num: usize) -> Result<Device<AnyDevice, AnyOS>> {
+        if num < 1 {
+            Err(Error::msg("num < 1 not allowed"))
+        } else {
+            let device = intern::ffi::fetch_device(num - 1)?;
+            if device.is_null() {
+                Err(Error::msg(format!("device {num} not found")))
+            } else {
+                Ok(Device {
+                    _num: num,
+                    dtype: AnyDevice(device),
+                    _os: PhantomData,
+                })
+            }
+        }
+    }
+
+    pub fn fetch_hsm(num: usize) -> Result<Device<Hsm, AnyOS>> {
+        if num < 1 {
+            Err(Error::msg("num < 1 not allowed"))
+        } else {
+            let device = intern::ffi::fetch_hsm(num - 1)?;
+            if device.is_null() {
+                Err(Error::msg(format!("HSM device {num} not found")))
+            } else {
+                Ok(Device {
+                    _num: num,
+                    dtype: Hsm(device),
+                    _os: PhantomData,
+                })
+            }
+        }
+    }
+}
+
+/// make methods like os() and dtype() available for Device
+impl<OS> std::ops::Deref for Device<AnyDevice, OS>
+where
+    OS: AnyDeviceOS,
+{
     type Target = intern::ffi::Device;
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.dtype.0
     }
 }
 
-impl std::ops::Deref for Hsm {
+/// make methods like os() and dtype() available for Hsm
+impl<OS> std::ops::Deref for Device<Hsm, OS>
+where
+    OS: AnyDeviceOS,
+{
     type Target = intern::ffi::HSM;
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.dtype.0
     }
 }
 
-pub fn fetch_device(num: usize) -> Result<Device> {
-    if num < 1 {
-        Err(Error::msg("num < 1 not allowed"))
-    } else {
-        let device = intern::ffi::fetch_device(num - 1)?;
-        if device.is_null() {
-            Err(Error::msg(format!("device {num} not found")))
-        } else {
-            Ok(Device(device))
-        }
-    }
-}
+// states
 
-pub fn fetch_hsm(num: usize) -> Result<Hsm> {
-    if num < 1 {
-        Err(Error::msg("num < 1 not allowed"))
-    } else {
-        let device = intern::ffi::fetch_hsm(num - 1)?;
-        if device.is_null() {
-            Err(Error::msg(format!("HSM device {num} not found")))
-        } else {
-            Ok(Hsm(device))
-        }
-    }
-}
+pub trait DeviceOS {}
+pub trait AnyDeviceOS: DeviceOS {}
+pub struct UnknownOS;
+pub struct AnyOS;
+pub struct BareMetal;
+pub struct Linux;
+pub struct WinDoof;
+impl DeviceOS for UnknownOS {}
+impl DeviceOS for AnyOS {}
+impl AnyDeviceOS for AnyOS {}
+impl DeviceOS for BareMetal {}
+impl AnyDeviceOS for BareMetal {}
+impl DeviceOS for Linux {}
+impl AnyDeviceOS for Linux {}
+impl DeviceOS for WinDoof {}
+impl AnyDeviceOS for WinDoof {}
+
+pub trait DeviceType {}
+pub trait AnyDeviceType: DeviceType {}
+pub struct UnknownDeviceType;
+pub struct AnyDevice(SharedPtr<intern::ffi::Device>);
+pub struct Hsm(SharedPtr<intern::ffi::HSM>);
+pub struct Fido;
+impl DeviceType for UnknownDeviceType {}
+impl DeviceType for AnyDevice {}
+impl AnyDeviceType for AnyDevice {}
+impl DeviceType for Hsm {}
+impl AnyDeviceType for Hsm {}
+impl DeviceType for Fido {}
+impl AnyDeviceType for Fido {}
 
 pub(super) mod intern {
     #[cxx::bridge(namespace = "cxx::device")]
@@ -77,7 +142,7 @@ pub(super) mod intern {
             #[cxx_name = "type"]
             fn dtype(self: &Device) -> DeviceType;
 
-            // Device
+            // HSM
             fn os(self: &HSM) -> DeviceOS;
             #[cxx_name = "type"]
             fn dtype(self: &HSM) -> DeviceType;
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use anyhow::Result;
 use cxx::Device;
 
 fn fetch_device(num: usize) {
-    let device: Device = cxx::fetch_device(num).expect("device found");
+    let device = Device::fetch_device(num).expect("device found");
     println!("Fetch device with num {num}:");
     println!("  Device OS: {:?}", device.os());
     println!("  Device Type: {:?}", device.dtype());
 }
 
 fn fetch_hsm(num: usize) {
-    match cxx::fetch_hsm(num) {
+    match Device::fetch_hsm(num) {
         Ok(device) => {
             println!("Fetch HSM device with num {num}:");
             println!("  Device OS: {:?}", device.os());
@@ -28,6 +28,11 @@ fn main() -> Result<()> {
     println!("Hello, world!");
 
     (1..=6).for_each(fetch_device);
+
+    // 0 -> Warning: num < 1 not allowed
+    // 4 -> Warning: HSM device 4 not found
+    // 5 -> Warning: HSM device 5 not found
+    // 6 -> Warning: HSM device 6 not found
     (0..=6).for_each(fetch_hsm);
 
     println!(".. no errors detected ..");
